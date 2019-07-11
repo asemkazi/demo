4c107c0f783ce2fbcc31b1a32d1ce5ad7b48dda5
Asem Kazi <Asem Kazi@DESKTOP-GQOM3LH>
2019-07-11 23:46:09 +0530

    First time commit

---
+++ Practice/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a=1__2__3__4.7_8_9f;
+		System.out.println(a);
+
+		System.out.println("Hllo");
+	}
+
+}
