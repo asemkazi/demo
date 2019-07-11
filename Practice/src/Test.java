8e40f5527db83052cb40a372ace324459d95ea24
Anuj Kawane <Anuj Kawane@Anuj>
2019-07-12 00:30:54 +0530

    nnjdk

--- Practice/src/Test.java
+++ Practice/src/Test.java
@@ -4,21 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		float a=1__2__3__4.7_8_9f;
-		System.out.println(a);
-
-		System.out.println("Hllo");
-		
-		
-		
-		System.out.println("Conflit msg");
-		System.out.println("Conflit msg 3");
-		deded
-//		adsdmsaldnsdsl
-//		asdasdss
-//		this is demo
-		
-		System.out.println("Conflit 2");
 	}
 
 }
